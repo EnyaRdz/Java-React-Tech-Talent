77e34b4a624260b8db22b8c5ffa4c571f220ccc6
EnyaRdz <enyarodriguez.98@gmail.com>
2025-02-21 13:17:26 +0100

    Update Tasque5_1.java

--- C2JavaBasic/src/Unidad05FlujoDatos/Tasque5_1.java
+++ C2JavaBasic/src/Unidad05FlujoDatos/Tasque5_1.java
@@ -1,21 +1,23 @@
 package Unidad05FlujoDatos;
+
+import java.util.Scanner;
+
 public class Tasque5_1 {
 	public static void main(String[] args) {
-		import java.util.Scanner;
 
-		        Scanner sc = new Scanner(System.in);
-		        System.out.print("Introduce el primer número: ");
-		        int a=83 = sc.nextInt();
-		        System.out.print("Introduce el segundo número: ");
-		        int b=37 = sc.nextInt();
-		        
-		        if (num1 > num2) {
-		            System.out.println(num1 + " es mayor que " + num2);
-		        } else if (num1 < num2) {
-		            System.out.println(num2 + " es mayor que " + num1);
-		        } else {
-		            System.out.println("Ambos números son iguales.");
-		        }
-		        sc.close();
-		    }
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduce el primer número: ");
+		int a = sc.nextInt();
+		System.out.print("Introduce el segundo número: ");
+		int b = sc.nextInt();
+
+		if (a > b) {
+			System.out.println(a + " es mayor que " + b);
+		} else if (a < b) {
+			System.out.println(b + " es mayor que " + a);
+		} else {
+			System.out.println("Ambos números son iguales.");
 		}
+		sc.close();
+	}
+}
