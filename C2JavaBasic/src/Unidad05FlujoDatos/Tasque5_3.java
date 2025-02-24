17b99ce9e3b11d7947be010c34da6bb79761a169
EnyaRdz <enyarodriguez.98@gmail.com>
2025-02-24 14:34:55 +0100

    5

--- C2JavaBasic/src/Unidad05FlujoDatos/Tasque5_3.java
+++ C2JavaBasic/src/Unidad05FlujoDatos/Tasque5_3.java
@@ -3,11 +3,8 @@ package Unidad05FlujoDatos;
 import javax.swing.JOptionPane;
 
 public class Tasque5_3 {
-
-	public static void main(String[] args) {
-		String nombre=JOptionPane.showInputDialog("Introduce tu nombre");
-		
-		JOptionPane.showMessageDialog(null, "Tu nombre es " +nombre);
-	}
-
+    public static void main(String[] args) {
+        String nombre = JOptionPane.showInputDialog("Introduce tu nombre:");
+        JOptionPane.showMessageDialog(null, "Bienvenido " + nombre);
+    }
 }
