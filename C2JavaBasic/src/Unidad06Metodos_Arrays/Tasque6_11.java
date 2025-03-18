ba4be54ba512f06f7b154890813d73f5117c0853
EnyaRdz <enyarodriguez.98@gmail.com>
2025-03-18 10:52:42 +0100

    Unidad 6 completa

--- C2JavaBasic/src/Unidad06Metodos_Arrays/Tasque6_11.java
+++ C2JavaBasic/src/Unidad06Metodos_Arrays/Tasque6_11.java
@@ -11,10 +11,10 @@ public class Tasque6_11 {
 		System.out.println("Introduce la dimensión de los arrays: ");
 		int dimension = sc.nextInt();
 
-		int[] array1 = rellenarArray1(dimension); // array1 se rellena de randoms con el método
+		int[] array1 = rellenarArray(dimension); // array1 se rellena de randoms con el método
 //		array1 = rellenarArray1(dimension);
 
-		int[] array2 = rellenarNumeros(dimension);
+		int[] array2 = rellenarArray(dimension);
 //		array2 = rellenarNumeros(dimension);
 
 		int[] multiplicado = multiplicarArrays(array1, array2);
@@ -36,10 +36,9 @@ public class Tasque6_11 {
 		}
 
 		sc.close();
-		
 	}
 
-	public static int[] rellenarArray1(int tamany) {
+	public static int[] rellenarArray(int tamany) {
 
 		Random aleatorio = new Random();
 		int[] arrayPrimero = new int[tamany];
@@ -51,17 +50,6 @@ public class Tasque6_11 {
 		// arrayPrimero = {8,5,3}
 
 		return arrayPrimero;
-	}
-
-	 public static int[] rellenarNumeros(int tamany) {
-	        Random aleatorio = new Random();
-	        int[] array = new int[tamany];
-
-	        for (int i = 0; i < tamany; i++) {
-	            array[i] = aleatorio.nextInt(11); // Genera números entre 0 y 10
-	        }
-
-	        return array;
 	}
 
 	public static int[] multiplicarArrays(int[] array1, int[] array2) {
