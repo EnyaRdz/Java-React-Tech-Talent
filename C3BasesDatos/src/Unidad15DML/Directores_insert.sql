e2b548fb246d7407a55cc3ea9c2ed027514ff648
EnyaRdz <enyarodriguez.98@gmail.com>
2025-05-26 13:56:04 +0200

    ud15

---
+++ C3BasesDatos/src/Unidad15DML/Directores_insert.sql
@@ -0,0 +1,14 @@
+
+USE DirectoresPeliculas;
+
+INSERT INTO Directores (nombre, nacionalidad, fecha_nacimiento) VALUES
+('Christopher Nolan', 'Británico', '1970-07-30'),
+('Pedro Almodóvar', 'Español', '1949-09-25'),
+('Greta Gerwig', 'Estadounidense', '1983-08-04'),
+('Sofia Coppola', 'Estadounidense', '1971-05-14'),
+('Alfonso Cuarón', 'Mexicano', '1961-11-28'),
+('Bong Joon-ho', 'Coreano', '1969-09-14'),
+('Patty Jenkins', 'Estadounidense', '1971-07-24'),
+('Guillermo del Toro', 'Mexicano', '1964-10-09'),
+('James Cameron', 'Canadiense', '1954-08-16'),
+('Denis Villeneuve', 'Canadiense', '1967-10-03');
---
+++ C3BasesDatos/src/Unidad15DML/Peliculas_insert.sql
@@ -0,0 +1,14 @@
+
+USE DirectoresPeliculas;
+
+INSERT INTO Peliculas (titulo, año, genero, id_director) VALUES
+('Inception', 2010, 'Ciencia Ficción', 1),
+('Dolor y Gloria', 2019, 'Drama', 2),
+('Barbie', 2023, 'Comedia', 3),
+('Lost in Translation', 2003, 'Drama', 4),
+('Gravity', 2013, 'Ciencia Ficción', 5),
+('Parásitos', 2019, 'Thriller', 6),
+('Wonder Woman', 2017, 'Acción', 7),
+('La forma del agua', 2017, 'Fantasía', 8),
+('Titanic', 1997, 'Romance', 9),
+('Dune', 2021, 'Ciencia Ficción', 10);
