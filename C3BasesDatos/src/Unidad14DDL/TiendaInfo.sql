faec9507fb0eaee3ac828c2b7afe8ae1f59d4494
EnyaRdz <enyarodriguez.98@gmail.com>
2025-05-27 12:00:42 +0200

    16

--- C3BasesDatos/src/Unidad14DDL/TiendaInfo.sql
+++ C3BasesDatos/src/Unidad14DDL/TiendaInfo.sql
@@ -1,26 +1,16 @@
-CREATE DATABASE TiendaInfo;
-USE TiendaInfo;
+CREATE DATABASE TiendaInformatica;
 
-CREATE TABLE Categorias (
-    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL UNIQUE,
-    descripcion TEXT
+USE TiendaInformatica;
+
+CREATE TABLE FABRICANTES (
+    Codigo INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre NVARCHAR(100)
 );
 
-CREATE TABLE Proveedores (
-    id_proveedor INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    email VARCHAR(100),
-    telefono VARCHAR(20)
-);
-
-CREATE TABLE Productos (
-    id_producto INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    tipo VARCHAR(50),
-    precio DECIMAL(10,2) NOT NULL,
-    id_categoria INT,
-    id_proveedor INT,
-    FOREIGN KEY (id_categoria) REFERENCES Categorias(id_categoria),
-    FOREIGN KEY (id_proveedor) REFERENCES Proveedores(id_proveedor)
+CREATE TABLE ARTICULOS (
+    Codigo INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre NVARCHAR(100),
+    Precio INT,
+    Fabricante INT,
+    FOREIGN KEY (Fabricante) REFERENCES FABRICANTES(Codigo)
 );
